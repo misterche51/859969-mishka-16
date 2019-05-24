d348d7ea1b1367be389d3e58575ba21ce908321a
misterche51 <alexanderchernykh88@gmail.com>
2019-05-24 06:31:26 +0300

    еще одна попытка

--- source/js/mishka.js
+++ source/js/mishka.js
@@ -1,9 +1,17 @@
 var buy = document.querySelector(".promo__buy");
+var buyCatalogVideo = document.querySelector(".catalog__video-button");
+var buyCatalogCart = document.querySelectorAll(".catalog__item-cart-link");
 var background = document.querySelector(".background");
-
 var popup = document.querySelector(".popup");
 
+
 buy.addEventListener("click", onPopupShow);
+background.addEventListener("click", onPopupClose);
+buyCatalogCart.addEventListener("click", onPopupShow);
+buyCatalogVideo.addEventListener("click", onPopupShow);
+
+
+
 
 
 function onPopupShow(evt) {
@@ -15,7 +23,8 @@ function onPopupShow(evt) {
 
 function onPopupClose(evt) {
   evt.preventDefault();
-  popup.remove("popup--show");
+  popup.classList.remove("popup--show");
+  background.classList.remove("background--active");
 }
 
 function onPopupEscapePress(evt) {
@@ -24,3 +33,23 @@ function onPopupEscapePress(evt) {
     window.removeEventListener("keydown", onPopupEscapePress);
   }
 }
+
+
+var menuBtn = document.querySelector(".nav__toggle-button")
+var menuListServ = document.querySelector(".nav__list--services");
+var menuListUser = document.querySelector(".nav__list--user");
+
+menuBtn.classList.remove("no-js");
+menuBtn.addEventListener("click", menuShow);
+
+
+function menuShow(evt) {
+  evt.preventDefault();
+  menuListServ.classList.toggle("nav__list--opened");
+  menuListUser.classList.toggle("nav__list--opened");
+  menuBtn.classList.toggle("nav__toggle-button--close");
+}
+
+
+
+
